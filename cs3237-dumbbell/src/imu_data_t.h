f1fa2207e00b1e145e4181edc14db429a2cf373e
neilbaner <git@neilbaner.net>
2022-11-07 22:38:44 +0800

    add azure iot code for wemos

---
+++ cs3237-dumbbell/src/imu_data_t.h
@@ -0,0 +1,8 @@
+typedef struct imu_data_t {
+    int acc_x;
+    int acc_y;
+    int acc_z;
+    int gyro_x;
+    int gyro_y;
+    int gyro_z;
+};
---
+++ cs3237-dumbbell/src/iot_configs.h
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "R2-D2"
+#define IOT_CONFIG_WIFI_PASSWORD "@mper$@nd"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "cs3237-test.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "cs3237-test-wemos"
+#define IOT_CONFIG_DEVICE_KEY "9BGcX/yrdY88cyQCp0Yl7RDGIv8l/OYhOk/434mEx9E="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
+#define NUM_SAMPLES 20
+
+#define BAUD_RATE 115200
